f054ba52e64bfb14d21d38391484bcaf7969ff49
unknown <thuylinh2490@gmail.com>
2019-06-19 22:05:44 +0700

    it works

---
+++ toantuScript.js
@@ -0,0 +1,9 @@
+let inputhWidth;
+let inputhHeight;
+inputhWidth =prompt("enter the width")
+inputhHeight = prompt("enter the height")
+
+let width = parseInt(inputhWidth);
+let height = parseInt(inputhHeight);
+let area = width * height
+document.write(area)
